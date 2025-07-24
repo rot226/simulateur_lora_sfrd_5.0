960ffd0eefd7fa0cafb6677da5bfc61645e8e78f
rot226 <160727202+rot226@users.noreply.github.com>
2025-07-24 15:15:46 +0000

    Add ADR and sensitivity unit tests

---
+++ launcher/tests/test_sensitivity.py
@@ -0,0 +1,8 @@
+import pytest
+from launcher.channel import Channel
+
+
+def test_channel_sensitivity_values():
+    channel = Channel()
+    expected = {7: -123, 8: -126, 9: -129, 10: -132, 11: -134.5, 12: -137}
+    assert channel.sensitivity_dBm == expected
