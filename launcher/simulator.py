e5c64a4d7b7ea670e163cae26a467f068d760714
rot226 <160727202+rot226@users.noreply.github.com>
2025-07-24 19:40:20 +0000

    Fix RX chain metrics and add regression tests

--- launcher/simulator.py
+++ launcher/simulator.py
@@ -242,7 +242,8 @@ class Simulator:
 
         # Compatibilité : premier canal par défaut
         self.channel = self.multichannel.channels[0]
-        self.network_server = NetworkServer(simulator=self, process_delay=0.001)
+        # Traiter immédiatement les paquets reçus pour éviter un retard artificiel
+        self.network_server = NetworkServer(simulator=self, process_delay=0.0)
         self.network_server.beacon_interval = self.beacon_interval
         self.network_server.beacon_drift = self.beacon_drift
         self.network_server.ping_slot_interval = self.ping_slot_interval
