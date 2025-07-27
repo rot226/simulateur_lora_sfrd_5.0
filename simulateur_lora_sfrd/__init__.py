d0396c3273ae141b455ac7a4feebe020abc58db6
rot226 <160727202+rot226@users.noreply.github.com>
2025-07-27 00:02:36 +0000

    Expose Node and server wrappers

--- simulateur_lora_sfrd/__init__.py
+++ simulateur_lora_sfrd/__init__.py
@@ -3,5 +3,15 @@
 from .mac import LoRaMAC
 from .phy import LoRaPHY
 from .application import Application
+from .loranode import Node
+from .gateway import Gateway
+from .network_server import NetworkServer
 
-__all__ = ["LoRaMAC", "LoRaPHY", "Application"]
+__all__ = [
+    "LoRaMAC",
+    "LoRaPHY",
+    "Application",
+    "Node",
+    "Gateway",
+    "NetworkServer",
+]
---
+++ simulateur_lora_sfrd/network_server.py
@@ -0,0 +1,5 @@
+"""Wrapper module exposing :class:`NetworkServer`."""
+
+from .launcher.server import NetworkServer
+
+__all__ = ["NetworkServer"]
