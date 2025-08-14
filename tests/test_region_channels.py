dceb5fb34c5db1c29b73ba4dc802b9d99afa8b56
rot226 <160727202+rot226@users.noreply.github.com>
2025-08-14 19:34:17 +0000

    Delete tests directory

--- tests/test_region_channels.py
+++
@@ -1,7 +0,0 @@
-from simulateur_lora_sfrd.launcher.channel import Channel
-
-
-def test_region_channels_accepts_region_in_kwargs():
-    channels = Channel.region_channels("EU868", region="ignored")
-    assert len(channels) == len(Channel.REGION_CHANNELS["EU868"])
-    assert all(ch.region == "EU868" for ch in channels)
